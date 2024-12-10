485b24a7c8ade904f8111fc1360841477b82f121
bulanov1741 <bulanov1741@gmail.com>
2024-12-10 19:34:22 +0300

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from PyQt6.QtWidgets import QApplication
+from pygame import K_w, K_d, K_s, K_a
 
 from player import Player
 
@@ -11,7 +12,7 @@ class Main():
 
         self.screen = pygame.display.set_mode((1600, 900), pygame.RESIZABLE)
         self.running = True
-        self.location_washer = 1 # 0 - ни у кого, 1 - у своей команды, 2 - у чужой команды
+        self.location_washer = 2 # 0 - ни у кого, 1 - у своей команды, 2 - у чужой команды
         # Игроки
         self.a1, self.a2, self.a3, self.a4, self.a5 = Player(self, 0, 5), Player(self, 5, 5), Player(self, 15, 5), \
             Player(self, 25, 5), Player(self, 35, 5)
@@ -21,6 +22,7 @@ class Main():
             Player(self, 35, 800), Player(self, 45, 800)
         self.players = [self.a1, self.a2, self.a3, self.a4, self.a5, self.b1, self.b2, self.b3, self.b4, self.b5,
                         self.goalkeeper1, self.goalkeeper2]
+        self.chosen_player = self.a1 # Выбранный игрок
 
         self.initUI()
 
@@ -34,9 +36,19 @@ class Main():
                 for i in self.players:
                     i.show()
 
+                if pygame.key.get_pressed()[K_w] or pygame.key.get_pressed()[K_a] or pygame.key.get_pressed()[K_s] or\
+                    pygame.key.get_pressed()[K_d]:
+                    self.chosen_player.move()
+
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.location_washer == 1:
-                        self.a1.broadcast(event)
+                        self.chosen_player.broadcast(event)
+
+                    elif self.location_washer == 2:
+                        for i in [self.a1, self.a2, self.a3, self.a4, self.a5]:
+                            if i.x + 100 >= event.pos[0] >= i.x and i.y + 100 >= event.pos[1] >= i.y:
+                                self.chosen_player = i
+                                break
 
                 if event.type == pygame.QUIT:
                     self.running = False
--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import K_w, K_s, K_a, K_d, K_q, K_e, K_RSHIFT
+
 
 class Player():
     def __init__(self, main, x, y, with_washer=0):
@@ -6,13 +8,27 @@ class Player():
         self.x, self.y, self.with_washer = x, y, with_washer
         self.player = pygame.image.load('player_image.png').convert_alpha()
 
-    def show(self):
+    def show(self): # Показываем
         self.main.screen.blit(self.player, (self.x, self.y))
+
+    def move(self):
+        moving = 5
+        if pygame.key.get_pressed()[K_RSHIFT]:
+            moving = 10  # Ускорение
+        if pygame.key.get_pressed()[K_w]:
+            self.y -= moving
+        if pygame.key.get_pressed()[K_s]:
+            self.y += moving
+        if pygame.key.get_pressed()[K_a]:
+            self.x -= moving
+        if pygame.key.get_pressed()[K_d]:
+            self.x += moving
 
     def throw(self): # Бросок
         pass
     def broadcast(self, event): # Передача
         print(event.pos)
+
 
 
     
